dfbe3e1d3364c8bc4ee01acc57b394eb3589a06c
Saroj Parida <SarojP@cogcis.onmicrosoft.com>
2018-03-02 13:12:22 +0000

    Add project files.

---
+++ ContosoAdsCommon/ContosoAdsContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContosoAdsCommon
+{
+    public class ContosoAdsContext : DbContext
+    {
+        public ContosoAdsContext() : base("name=ContosoAdsContext")
+        {
+        }
+
+        public ContosoAdsContext(string connString) : base(connString)
+        {
+        }
+
+        public System.Data.Entity.DbSet<Ad> Ads { get; set; }
+
+    }
+}
